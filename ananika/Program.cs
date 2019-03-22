b7607845a787eb1a888f5f7c2555b6d83629e1a4
ivanmarin1 <48826053+ivanmarin1@users.noreply.github.com>
2019-03-22 12:02:09 +0100

    Update Program.cs
    Ivan

--- ananika/Program.cs
+++ ananika/Program.cs
@@ -8,6 +8,7 @@ namespace ananika
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Ovo je Ivan napisa!");
         }
     }
 }
