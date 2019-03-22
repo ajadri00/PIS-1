836d134af239522c26d13de1045356f6ca69b9da
ajadri00 <ajadri00@fesb.hr>
2019-03-22 11:42:40 +0100

    First commit

---
+++ ananika/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ananika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
